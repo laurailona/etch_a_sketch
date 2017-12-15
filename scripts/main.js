ad6e37d375d7f90b2ff258227fe50434cd190867
Laura Ilona <clarkelaura92@gmail.com>
2017-12-15 13:29:52 +0200

    Create random colors on hover, darken on hover, check if prompt input is correct

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,15 @@
 /* 1 - CREATE THE GRID */
 var gridSize = prompt("How big do you want your grid to be?\ne.g. '8' will create an 8x8 grid", "8");
-
+var checkInput = function() {
+  while(gridSize.isNaN || gridSize % 1 !== 0 || gridSize > 200 || gridSize < 0) {
+  gridSize = prompt("Try again! Please use only whole numbers (integers) from 0 - 200!");
+  }
+};
 //Create grid
 var container = document.getElementById("container");
 
 var createGrid = function () {
+  checkInput();
   for(let rowPosition = 1; rowPosition <= gridSize; rowPosition++) {
     let newDiv = document.createElement("div");
     newDiv.className = "row";
@@ -20,16 +25,24 @@ var createGrid = function () {
     };
   };
 };
-
 createGrid();
 
 //Change color on hover
-
 var drawOnHover = function() {
   let squares = document.querySelectorAll(".col");
   squares.forEach((div) => {
-    div.addEventListener("mouseover", (e) => {
-      div.classList.add("change-color");
+    let rgbArray = [];
+    function makeRandomColor(){
+      let rgb1 = Math.floor(Math.random() * 256);
+      let rgb2 = Math.floor(Math.random() * 256);
+      let rgb3 = Math.floor(Math.random() * 256);
+      rgbArray = [(rgb1 + 30), (rgb2 + 30), (rgb3 + 30)]; //+30 ensures returning 'original' value initially in next step
+    };
+    makeRandomColor();
+    div.addEventListener("mouseout", (e) => {
+      rgbArray = rgbArray.map(x => x - 30);
+      let squareColor = "background-color: rgb(" + rgbArray.join(", ") + ")";
+      div.setAttribute("style", squareColor);
     });
   });
 }
@@ -45,4 +58,3 @@ resetButton.addEventListener("click", (e) => {
   createGrid();
   drawOnHover();
 });
-
